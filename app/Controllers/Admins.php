ded55705db59957615df3bfd5ef2ac66a861a7c5
Shandy Dailami <shandydailami@gmail.com>
2025-01-28 22:54:35 +0800

    update dashboard

---
+++ app/Controllers/Admins.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\User;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Admins extends BaseController
+{
+    public function index()
+    {
+        return view('admin/signin', ['title' => 'Admin']);
+    }
+
+    public function signinAdmin()
+    {
+        helper('form');
+
+        if (
+            $this->validate([
+                'username' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'Username cannot be empty.'
+                    ]
+                ],
+                'password' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'Passsword cannot be empty',
+                    ]
+                ],
+                'role' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => 'Role cannot be empty.',
+                    ]
+                ]
+            ])
+        ) {
+            $username = $this->request->getPost('username');
+            $password = $this->request->getPost('password');
+            $role = $this->request->getPost('role');
+
+            $model = new User();
+            $admin = $model->where('username')->first();
+
+            if ($admin && password_verify($password, $admin['password'])) {
+                session()->set('id', $admin['id']);
+                session()->setFlashdata('message', 'You have successfully signed in');
+
+                return view('/adminPanel');
+            } else {
+                session()->setFlashdata('error', 'Wrong username or password');
+                return redirect()->to('admin/signin');
+            }
+        } else {
+            return redirect()->to('/admin/signin')->withInput()->with('errors', $this->validator->getErrors());
+        }
+    }
+
+    public function signupAdminPage()
+    {
+        return view('admin/signup', ['title' => 'Admin Sign Up']);
+    }
+}
